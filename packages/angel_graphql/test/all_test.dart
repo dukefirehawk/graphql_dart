6b6f7d80a0312087a8cc281c9a0d6e14132045e6
Thomas <thomashii@dukefirehawk.com>
2025-09-14 11:47:27 +0800

    Updated tests

---
+++ packages/angel_graphql/test/all_test.dart
@@ -0,0 +1,8 @@
+//import 'dart:async';
+
+//import 'package:angel3_graphql/angel3_graphql.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('test', () async {});
+}
---
+++ packages/graphql_generator/test/all_test.dart
@@ -0,0 +1,7 @@
+//import 'dart:async';
+//import 'package:graphql_generator2/graphql_generator2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('test', () async {});
+}
